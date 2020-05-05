411248bc9c23406d16bedac0476cb6ee5f8b201f
KseniaMV <marks-83@mail.ru>
2020-05-05 17:36:10 +0300

    Add function turnCard

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,5 @@
-let cardBack = document.querySelector(".prov2");
-let cardFace = document.querySelector(".prov");
+
 let buttonStartGame = document.querySelector(".button_start_game");
-
-/*cardBack.addEventListener("click", function () { //animate rotation to front side
-    console.log("re");
-    cardFace.style.transform = "rotateY(0)";
-    cardBack.style.transform = "rotateY(180deg)";
-
-});
-
-
-cardFace.addEventListener("click", function () {
-    console.log("ds"); //animate rotation to front side
-    cardFace.style.transform = "rotateY(-180deg)"; //animate rotation to back side
-    cardBack.style.transform = "rotateY(0)";
-
-});
-*/
 
 buttonStartGame.addEventListener("click", () => {
     let xhr = new XMLHttpRequest();
@@ -24,22 +7,53 @@ buttonStartGame.addEventListener("click", () => {
     xhr.send();
     xhr.onload = function () {
         if (xhr.status != 200) { 
-            alert('Ошибка: ' + xhr.status);
+            document.querySelector(".content").innerHTML = 'Ошибка: ' + xhr.status;
             return;
         }else{
             let response = xhr.response;
             document.querySelector(".content").innerHTML = (response);
+            turnCard();
         }
-
-        // получим ответ из xhr.response
     };
 
     xhr.onprogress = function (event) {
-        // выведем прогресс
-        alert(`Загружено ${event.loaded} из ${event.total}`);
+
+        document.querySelector(".content").innerHTML = `Загружено ${event.loaded} из ${event.total}`;
     };
 
     xhr.onerror = function () {
         alert("job,rf");
     };
-})
+});
+
+function turnCard(){
+    let cards = document.querySelectorAll(".card")
+    let cardBack = document.querySelectorAll(".card_back");
+    let cardFace = document.querySelectorAll(".card_face");
+   cardFace.forEach(cardFace => {
+       cardFace.addEventListener("click", (e)=>{
+           let targetFace = e.target;
+           let targetBack = e.target.nextSibling;
+           targetFace.style.transform = "rotateY(-180deg)";
+           targetBack.nextSibling.style.transform = "rotateY(0)";
+       })
+       
+   });
+           
+           
+            
+}
+
+
+/*cardBack.addEventListener("click", function () { //animate rotation to front side
+    console.log("re");
+    cardFace.style.transform = "rotateY(0)";
+    cardBack.style.transform = "rotateY(180deg)";
+}
+
+cardFace.addEventListener("click", function () {
+    console.log("ds"); //animate rotation to front side
+    cardFace.style.transform = "rotateY(-180deg)"; //animate rotation to back side
+    cardBack.style.transform = "rotateY(0)";
+
+});*/
