f9d1b4c88f2270d8620f565ae65039df36620d8d
KseniaMV <marks-83@mail.ru>
2020-05-10 14:43:23 +0300

    create new structure, getImages, shuffle, timer

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,14 @@
 let buttonStartGame = document.querySelector(".button_start_game");
 let gameContent = document.querySelector(".content");
+let cardImages = [];
 
+/*download page with rules*/
 buttonStartGame.addEventListener("click", () => {
     getPage('components/rules_page/rules_page.html').then(letsPlay);
     buttonStartGame.style.display = "none";
 });
 
-
+/*start game, download page with game table*/
 function letsPlay() {
     let buttonGo = document.querySelector(".game_rules_button-go");
     buttonGo.addEventListener("click", () => {
@@ -15,72 +17,7 @@ function letsPlay() {
         previouscontent.forEach(element => {
             element.remove();
         });
-        getPage('components/game_plate/game_plate.html').then(turnCard).then(timeOut);
+        getPage('components/game_plate/game_plate.html').then(getImages).then(shuffle).then(createCards).then(turnCard).then(timeOut);
     });
 
 };
-
-
-function turnCard() {
-    let cardBack = document.querySelectorAll(".card_back");
-    let cardFace = document.querySelectorAll(".card_face");
-    cardFace.forEach(cardFace => {
-        cardFace.addEventListener("click", (e) => {
-            let targetFace = e.target;
-            let targetBack = targetFace.nextElementSibling;
-            targetFace.style.transform = "rotateY(-180deg)";
-            targetBack.style.transform = "rotateY(0)";
-        })
-
-    });
-
-    cardBack.forEach(cardBack => {
-        cardBack.addEventListener("click", (e) => {
-            let targetBack = e.target;
-            let targetFace = e.target.previousElementSibling;
-            targetBack.style.transform = "rotateY(180deg)";
-            targetFace.style.transform = "rotateY(0)";
-        })
-
-    });
-
-
-
-};
-
-
-function getPage(url) {
-    return new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest();
-        request.open("GET", url);
-        request.onload = function() {
-            try {
-                if (this.status === 200) {
-                    resolve(gameContent.innerHTML = this.response);
-                }else {
-                    reject(this.status + "" + this.statusText);
-                }
-            } catch(e) {
-                console.log(e);
-            }
-        }
-
-        request.onerror = function () {
-            reject(this.status + "" + this.statusText);
-        }
-
-        request.onprogress = function (event) {
-            gameContent.innerHTML = `Загружено ${event.loaded} из ${event.total}`;
-        };
-        request.send();
-    });
-
-};
-
-function timeOut(){
-    let timeCount = 60;
-    let timeCountTime = document.querySelector(".timeCount_time");
-        setInterval(() => {
-            timeCountTime.innerHTML = --timeCount;
-        }, 1000);
-}
